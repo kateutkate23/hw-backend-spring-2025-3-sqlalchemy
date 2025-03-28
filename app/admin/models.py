0421115588f814b87e8100e23fd69eae83866304
Ekaterina Utkina <kate_utkate@vk.com>
2025-03-28 19:13:04 +0300

    add first migration

--- app/admin/models.py
+++ app/admin/models.py
@@ -1,5 +1,13 @@
+from typing import Optional
+
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
 from app.store.database.sqlalchemy_base import BaseModel
 
 
 class AdminModel(BaseModel):
     __tablename__ = "admins"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(String(128), unique=True)
+    password_hash: Mapped[Optional[str]] = mapped_column(String(128))
--- app/quiz/models.py
+++ app/quiz/models.py
@@ -1,13 +1,30 @@
+from sqlalchemy import String, Boolean, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
 from app.store.database.sqlalchemy_base import BaseModel
 
 
 class ThemeModel(BaseModel):
     __tablename__ = "themes"
-
-
-class QuestionModel(BaseModel):
-    __tablename__ = "questions"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(128), unique=True)
+    questions: Mapped[list["QuestionModel"]] = relationship(
+        back_populates="theme", cascade="all, delete-orphan"
+    )
 
 
 class AnswerModel(BaseModel):
     __tablename__ = "answers"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(128))
+    is_correct: Mapped[bool] = mapped_column(Boolean)
+    question_id: Mapped[int] = mapped_column(ForeignKey("questions.id"))
+
+
+class QuestionModel(BaseModel):
+    __tablename__ = "questions"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(128), unique=True)
+    theme_id: Mapped[int] = mapped_column(ForeignKey("themes.id"))
+    theme: Mapped["ThemeModel"] = relationship(back_populates="questions")
+    answers: Mapped[list["AnswerModel"]] = relationship(cascade="all, delete-orphan")
